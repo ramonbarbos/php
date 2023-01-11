5edbee109bc07c928205546eaa0ee10673432af2
unknown <ramonlegendario21@gmail.com>
2023-01-11 08:33:00 -0300

    conexao estruturada

---
+++ BancoDeDados/conexao.php
@@ -0,0 +1,17 @@
+<?php
+
+define('HOST', 'localhost');
+define('DBNAME', 'primeirobanco');
+define('USER', 'root');
+define('PASS', '00000');
+
+
+try {
+    $pdo = new PDO('mysql:host=' . HOST . ';dbname=' . DBNAME, USER, PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    echo '<h3>Falha ao conectar ao Banco de Dados!</h3>';
+}
+
+
+?>
