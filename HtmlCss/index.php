2a050a4d264b523c46489afbd3fa53ec8c716772
unknown <ramonlegendario21@gmail.com>
2023-01-17 17:38:37 -0300

    user online

--- HtmlCss/index.php
+++ HtmlCss/index.php
@@ -1,7 +1,7 @@
 <?php include('config.php' ); ?>
 <?php include('./painel/classes/Site.php' ); Site::updateUsuarioOnline();?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
--- HtmlCss/painel/classes/Site.php
+++ HtmlCss/painel/classes/Site.php
@@ -1,21 +1,21 @@
 <?php 
-    include('MySql.php' );
-
+   include('MySql.php' );
+  
     class Site {
 
         public static function updateUsuarioOnline(){
             if(isset($_SESSION['online'])){
-                $token = $_SESSION['online'];
-                $horarioAtual = date('Y-m-d H:i:s');
-                $sql = MySql::conectar() ->prepare("UPDATE `tb_admin.online` SET ultima_acao = ? WHERE token = ? ");
-                $sql->execute(array($horarioAtual,$token));
+              $token = $_SESSION['online'];
+              $horarioAtual = date('Y-m-d H:i:s');
+              $sql = MySql::conectar()->prepare("UPDATE `tb_admin.online` SET ultima_acao = ? WHERE token = ? ");
+              $sql->execute(array($horarioAtual,$token));
             }else{
                 $_SESSION['online'] = uniqid();
-                $ip = $_SESSION['REMOTE_ADDR'];
+                $ip = $_SERVER['REMOTE_ADDR'];
                 $token = $_SESSION['online'];
                 $horarioAtual = date('Y-m-d H:i:s');
-                $sql = MySql::conectar() ->prepare("INSERT INTO  `tb_admin.online` VALUES (null, ?,?) ");
-                $sql->execute(array($horarioAtual,$token));
+                $sql = MySql::conectar() ->prepare("INSERT INTO  `tb_admin.online` VALUES (null, ?,?,?) ");
+                $sql->execute(array($ip,$horarioAtual,$token));
 
             }
         }
