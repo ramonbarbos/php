d586ce70263f68a9fc7c6c88949d0d1ccf5ce64b
Ramon Barbosa <ramonlegendario21@gmail.com>
2023-01-23 20:05:30 -0300

    add pibic

---
+++ pibic/dados.js
@@ -0,0 +1,54 @@
+export const data = [{
+  "type": "Feature",
+  "properties": {
+      "name": "Coors Field",
+      "amenity": "Baseball Stadium",
+      "popupContent": "This is where the Rockies play!"
+  },
+  "geometry": {
+      "type": "Point",
+      "coordinates": [-38.456569, -12.978306 ],
+  }
+},
+{
+  "type": "Feature",
+  "properties": {
+      "name": "Coors Field",
+      "amenity": "Baseball Stadium",
+      "popupContent": "This is where the Rockies play!"
+  },
+  "geometry": {
+      "type": "Point",
+      "coordinates": [-38.4544, -12.981777 ],
+  }
+},
+{
+  "type": "Feature",
+  "properties": {
+      "name": "Coors Field",
+      "amenity": "Baseball Stadium",
+      "popupContent": "This is where the Rockies play!"
+  },
+  "geometry": {
+      "type": "Point",
+      "coordinates": [-38.454432, -12.979646 ], 
+
+
+  }
+},
+{
+  "type": "Feature",
+  "properties": {
+      "name": "Coors Field",
+      "amenity": "Baseball Stadium",
+      "popupContent": "This is where the Rockies play!"
+  },
+  "geometry": {
+      "type": "Point",
+      "coordinates": [-38.451488, -12.979090 ], 
+
+
+  }
+},
+];
+
