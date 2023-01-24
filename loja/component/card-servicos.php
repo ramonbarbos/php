91acdf3578b8a195c60a6202defcd0df29724795
unknown <ramonlegendario21@gmail.com>
2023-01-24 13:34:54 -0300

    css loja

--- loja/component/card-servicos.php
+++ loja/component/card-servicos.php
@@ -1,15 +1,38 @@
-<div class="content-img">
-    <div class="img"></div>
-</div>
-<div class="content-servicos">
 
-    <!--Primeiro Serviço-->
-    <div class="card" style="width: 18rem;">
-        <div class="card-body">
-          <h5 class="card-title">Special title treatment</h5>
-          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+
+<div id="container-servicos" class="container">
+
+  <div class="content-img">
+      <div  id="img-servicos" class="img" >Foto</div>
+  </div>
+
+  <div class="content-servicos">
+
+      <!--Primeiro Serviço-->
+      <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">FOTOGRAFIA PROFISIONAL</h5>
+            <p class="card-text">Oferecemos diversos serviços de fotografia, estúdio profisional, fotos para documentos, cobertura de eventos.</p>
+          </div>
         </div>
-      </div>
+           <!--Primeiro Serviço-->
+      <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">FOTOGRAFIA PROFISIONAL</h5>
+            <p class="card-text">Oferecemos diversos serviços de fotografia, estúdio profisional, fotos para documentos, cobertura de eventos.</p>
+          </div>
+        </div>
+           <!--Primeiro Serviço-->
+      <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">FOTOGRAFIA PROFISIONAL</h5>
+            <p class="card-text">Oferecemos diversos serviços de fotografia, estúdio profisional, fotos para documentos, cobertura de eventos.</p>
+          </div>
+        </div>  
+        
+
+  </div>
+
+  
 
 </div>
--- loja/index.php
+++ loja/index.php
@@ -7,8 +7,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
+    <!--CSS -->
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/servicos.css">
+    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
     <title><?php echo NOME_SITE; ?></title>
