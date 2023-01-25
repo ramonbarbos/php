a6b74495fd51be4fb0b9de1041302866ec20e098
unknown <ramonlegendario21@gmail.com>
2023-01-25 16:47:14 -0300

    nav painel

---
+++ loja/painel/component/nav.php
@@ -0,0 +1,35 @@
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+ 
+  
+
+    <a class="navbar-brand" href="#">Loja</a>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    
+    <div class="div" style=" width: 100%;display:flex; justify-content: end;"><!-- Inicio --> 
+    
+      <ul class="navbar-nav" >
+
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Serviços<span class="sr-only">(current)</span></a>
+        </li>
+        
+        <li class="nav-item">
+        <i class="fa fa-sign-out" aria-hidden="true"></i>
+          <a class="nav-link" href="<?php echo INCLUDE_PATH_PAINEL ?>?logout">Sair</a>
+        </li>
+      
+      </ul>
+ 
+
+   
+
+  </div><!-- Fim -->
+
+</nav>
+
