aec4dfa936f36571e0e330dd173ed4ba610de01d
Ramon Barbosa <ramonlegendario21@gmail.com>
2023-02-22 21:20:22 -0300

    select categoria

--- loja/page/noticia.php
+++ loja/page/noticia.php
@@ -6,55 +6,114 @@
   $cat = MySql::conectar()->prepare("SELECT * FROM `tb_site.categoria` WHERE slug = ?");
   $cat->execute(array(@$url[1]));
   $cat = $cat->fetch();
-    print_r($cat);
+    //print_r($cat);
    //$cat['nome'];
 ?>
 
-<div id="legendas" class="container-fluid">
-     <?php 
-       
-          
-        /* 
-           if($cat['nome'] == ''){
-            echo '<div id="legendas" class="container-fluid">
-                  <p>Todas as Noticias</p>
-              </div>';
-          }else{
-            echo '<div id="legendas" class="container-fluid">
-            <p>Novas Noticias em '.$cat['nome'].'</p>
-          </div>';
-          }
- 
-          $query = "SELECT * FROM `tb_site.noticias`";
-          if($cat['nome'] !=''){
-            $query.="WHERE categoria_id = $cat[id]";
-          }
-          $sql =  MySql::conectar()->prepare($query);
-          $sql->execute();
-          $noticias = $sql->fetchAll();
-    */  ?>
+
+   
     <div id="legendas" class="container-fluid">
                   <p>Todas as Noticias</p>
               </div>
-     </div>
+     
 
 
-    <section id="painel-noticia" class="container mt-5">
+    <section id="painel-noticia" class="container mt-5"> 
 
      
 
-        <div id="lateral" class="container-fluid" >
-     <?php Componente::lateralNoticias(); ?>
-    </div>
+        <div id="lateral" class="container-fluid" ><!--INICIO LATERAL-->
+                          
+                <div id="container-lateral" >
+
+                <div class="caixa mt-4 mb-5" style="width: 18rem; height: 10rem;background-color:  #c6cecf">
+
+                    <div class="titulo mt-3">
+                        Realizar uma busca:
+                    </div>
+
+                    <form  role="search">
+                    <input class="form-control mt-2" type="search" name="parametro" placeholder="O que deseja procurar?" aria-label="Search">
+                    <button class="btn btn-outline-success mt-3" type="submit">Pesquisar</button>
+                    </form>
+
+                </div>
 
 
-     <div id="noticias" class="container-fluid" >
-     <?php Componente::carregarNoticias(); ?>
-    </div>
+                <div class="caixa mt-4 mb-4" style="width: 18rem; height: 8rem;">
+
+                    <div class="titulo mt-3">              
+                        Selecionar categoria:
+                    </div>
+                  
+                    <form >
+                        <select class="form-control mt-2"  name="categoria">
 
 
+                        <option value="" disabled selected="">Todas as categorias</option>
 
-    </section>
+                        <?php
+                            $categorias = Painel::selectAll('tb_site.categoria');
+                            foreach($categorias as $key => $value) {
+
+                        ?>
+                          <option <?php if($value['slug'] == @$url[1]) echo 'selected'; ?> value="<?php echo $value['slug'] ?>"><?php echo $value['nome']; ?></option>
+                          
+                          <?php } ?>
+                      
+                      
+                        </select>
+
+                    </form>
+
+                </div>
+
+                </div>
+       </div><!--FIM LATERAL-->
+
+
+    <div id="container-noticias" ><!--INICIO NOTICIAS-->
+
+          
+              
+              <?php 
+                  
+
+                  $query = "SELECT * FROM `tb_site.noticias` ";
+                  if($cat['nome'] !=''){
+                    $query.="WHERE categoria_id = $cat[id]";
+
+                  }
+                  $sql =  MySql::conectar()->prepare($query);
+                  $sql->execute();
+                  $noticias = $sql->fetchAll();
+                ?>
+                <?php 
+                    foreach($noticias as $key => $value) {
+                      $sql =  MySql::conectar()->prepare("SELECT  `slug` FROM  `tb_site.categoria` WHERE id = ? ");
+                      $sql->execute(array($value['categoria_id']));
+                      $categoriaNome = $sql->fetch()['slug'];
+                  ?>
+                <div class="card mt-4 mb-4" style="width: 100%;">
+                <div class="card-body">
+
+                  <h5 class="card-title"><?php echo $value['titulo'];?></h5>
+
+                  <h6 class="card-subtitle mb-2 text-muted"><?php echo date('d/m/Y',strtotime($value['data']));?></p>
+                  
+                  <p class="card-text"><?php echo substr($value['conteudo'],0,300).'...';?></p>
+
+                  <a href="<?php echo INCLUDE_PATH; ?>noticia/<?php echo $categoriaNome; ?>/<?php echo $value['slug']; ?>" class="card-link">Link da Noticia</a>
+                </div>
+                </div>
+          <?php } ?>
+
+
+          </div> <!--FIM NOTICIAS-->
+
+
+    </section> <!--FIM TOTAL-->
+
 <?php }else{
     include('page/noticia_single.php');
 } ?>
